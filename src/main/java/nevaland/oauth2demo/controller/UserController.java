7fd97d9840d5462fc792d1ef9b5420a98601a4c8
Nevaland <deanh0716@gmail.com>
2020-11-20 14:52:05 +0900

    Refactoring principal parameter

--- src/main/java/nevaland/oauth2demo/controller/UserController.java
+++ src/main/java/nevaland/oauth2demo/controller/UserController.java
@@ -1,16 +1,17 @@
 package nevaland.oauth2demo.controller;
 
+import nevaland.oauth2demo.oauth2.KakaoOAuth2User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 public class UserController {
 
     @RequestMapping("/user")
-    public Principal user(Principal principal) {
-        System.out.println(principal);
-        return principal;
+    public KakaoOAuth2User user(@AuthenticationPrincipal KakaoOAuth2User kakaoOAuth2User) {
+        System.out.println(kakaoOAuth2User);
+        return kakaoOAuth2User;
     }
+
 }
