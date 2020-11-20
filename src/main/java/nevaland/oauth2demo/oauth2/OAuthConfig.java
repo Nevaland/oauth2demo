1ff10f8155fe52fb3335e65254189cafb323e4d7
Nevaland <deanh0716@gmail.com>
2020-11-20 16:39:01 +0900

    Implement logout

--- src/main/java/nevaland/oauth2demo/oauth2/OAuthConfig.java
+++ src/main/java/nevaland/oauth2demo/oauth2/OAuthConfig.java
@@ -3,6 +3,7 @@ package nevaland.oauth2demo.oauth2;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class OAuthConfig extends WebSecurityConfigurerAdapter {
@@ -13,6 +14,10 @@ public class OAuthConfig extends WebSecurityConfigurerAdapter {
                 oauth2Login -> oauth2Login.userInfoEndpoint(
                         userInfoEndpoint -> userInfoEndpoint.customUserType(KakaoOAuth2User.class, "kakao")
                 )
-        );
+            )
+            .logout()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+            .logoutSuccessUrl("/")
+            .invalidateHttpSession(true);
     }
 }
