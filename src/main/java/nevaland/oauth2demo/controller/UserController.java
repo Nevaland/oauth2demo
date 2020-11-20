12e00d45fa1427523d6c8237260d49648350d164
Nevaland <deanh0716@gmail.com>
2020-11-20 19:58:12 +0900

    Implement nickname setting with memory repository

--- src/main/java/nevaland/oauth2demo/controller/UserController.java
+++ src/main/java/nevaland/oauth2demo/controller/UserController.java
@@ -1,5 +1,10 @@
 package nevaland.oauth2demo.controller;
 
+import nevaland.oauth2demo.domain.User;
+import nevaland.oauth2demo.oauth2.KakaoOAuth2User;
+import nevaland.oauth2demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,15 +13,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class UserController {
 
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/user/new")
     public String newUser() {
         return "setNicknameForm";
     }
 
     @PostMapping("/user/new")
-    public String registerUser(@RequestParam(value = "nickname") String nickname) {
-        System.out.println(nickname);
-        return "redirect:/index";
+    public String registerUser(@AuthenticationPrincipal KakaoOAuth2User kakaoOAuth2User,
+                               @RequestParam(value = "nickname") String nickname) {
+        if(kakaoOAuth2User != null) {
+            User user = new User(Long.parseLong(kakaoOAuth2User.getName(), 10), nickname);
+            userService.join(user);
+        }
+        return "redirect:/";
     }
 
 }
